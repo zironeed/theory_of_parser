c675dfaed473259e3acd084716c64890ff549dc7
Adil S <forever@nzironeed.ru>
2024-03-15 21:09:10 +0500

    some changes

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from bs4 import BeautifulSoup
-import requests
-import re
-
-from services.get_pages import get_page_numbers
-from services.get_request import get_requests
+from services.get_and_save import get_page_numbers, get_requests
 
 
 def main():
     links = get_page_numbers('https://easyoffer.ru/rating/python_developer')
-    get_requests(links)
+    questions = get_requests(links)
+
+    print(questions)
 
 
 if __name__ == '__main__':
