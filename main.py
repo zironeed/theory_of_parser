468d62ab02950f7ab01d0ad80e84d3380c665eb3
Adil Salimgareev <forever@nzironeed.ru>
2024-08-15 20:38:06 +0500

    deleted sync parser

--- main.py
+++ main.py
@@ -2,36 +2,18 @@ import asyncio
 from os import getenv
 from time import time
 from dotenv import load_dotenv
-
-from services.get_and_save import get_page_numbers, get_requests, save_to_csv, get_categories   # sync
-
-from services_async import get_info_from_page, save_info                                        # async
+from services_async import get_info_from_page, save_info
 
 
 env_path = '/.env'
 load_dotenv(dotenv_path=env_path)
 
 
-def main():
-    start = time()
-
-    url = getenv('MAIN_URL')
-
-    links = get_page_numbers(url)
-    categories = get_categories(url)
-
-    questions = get_requests(links)
-
-    save_to_csv(questions, categories)
-
-    print(time()-start)
-
-
 async def async_main():
     start = time()
     links, categories = [], []
 
-    url = 'https://easyoffer.ru/rating/python_developer'
+    url = getenv('MAIN_URL')
     pages = await get_info_from_page.get_pagination(url)
 
     print('getting categories and links...')
@@ -51,7 +33,4 @@ async def async_main():
 
 
 if __name__ == '__main__':
-    if int(input("0 - sync, 1 - async\n")):
-        asyncio.run(async_main())
-    else:
-        main()
+    asyncio.run(async_main())
