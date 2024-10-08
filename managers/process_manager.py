271077391678646cc64760f580d980586cf079df
Adil Salimgareev <forever@nzironeed.ru>
2024-10-08 19:34:27 +0500

    added a new headache

---
+++ managers/process_manager.py
@@ -0,0 +1,12 @@
+class ProcessManager:
+    def __init__(self):
+        pass
+
+    async def parse_process(self):
+        pass
+
+    async def save_process(self):
+        pass
+
+    async def main_process(self):
+        pass
