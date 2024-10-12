86b0845898307293959c36e94a883d1e537d03a2
Adil Salimgareev <forever@nzironeed.ru>
2024-10-12 21:32:47 +0500

    process_manager: - added init method

--- managers/process_manager.py
+++ managers/process_manager.py
@@ -1,6 +1,22 @@
+from parse_manager import ParseManager
+from db_manager import DatabaseManager
+
+
 class ProcessManager:
-    def __init__(self):
-        pass
+    """
+    Класс, объединяющий процессы парсинга и сохранения данных
+    """
+    def __init__(self, database: str, user: str, password: str, host: str, main_url: str, secondary_url: str) -> None:
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+
+        self.main_url = main_url
+        self.secondary_url = secondary_url
+
+        self.parse_manager = ParseManager(main_url, secondary_url)
+        self.db_manager = DatabaseManager(database, user, password, host)
 
     async def parse_process(self):
         pass
