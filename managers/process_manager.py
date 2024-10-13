67a4c4ca2f6a681270754ad3b9aa6ff6a158b9fc
Adil Salimgareev <forever@nzironeed.ru>
2024-10-13 21:17:23 +0500

    process_manager: - added save to csv method

--- managers/process_manager.py
+++ managers/process_manager.py
@@ -1,3 +1,5 @@
+import csv
+
 from parse_manager import ParseManager
 from db_manager import DatabaseManager
 
@@ -23,6 +25,31 @@ class ProcessManager:
 
     async def save_process(self):
         pass
+
+    @staticmethod
+    def save_to_csv(questions: list, categories: list):
+        """
+        Сохраняем вопросики в CSV-файл
+        :param questions: вопросы
+        :param categories: список категорий
+        :return: пусто, сохранение инфы
+        """
+        print('saving questions to a CSV-file...')
+
+        file_name = 'data.csv'
+
+        with open(file_name, 'w', newline='', encoding='UTF-8') as file:
+            fieldnames = ['question', 'answer', 'category']
+
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for ind in range(len(questions)):
+                writer.writerow({
+                    'question': questions[ind][0],
+                    'answer': questions[ind][1],
+                    'category': categories[ind]
+                })
 
     async def main_process(self):
         pass
